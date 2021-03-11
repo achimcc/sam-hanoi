6b8a0d11008d19375af213ec9858af6dfc7554e8
Achim <achim.schneider@posteo.de>
2021-03-11 16:11:09 +0100

    elise midi

--- src/components/Solved/Solved.tsx
+++ src/components/Solved/Solved.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import styled from "styled-components";
 import dispatch from "../../sam/dispatch";
+import MidiPlayer from "midi-player-ts";
 
 const SolvedDiv = styled.div`
   color: rgba(255, 255, 255, 0.75);
@@ -16,18 +16,41 @@ interface Props {
   model: Model;
 }
 
+let midi;
+
+const load = async (player: any) => {
+  player.stop();
+  midi = await fetch("./elise.mid").then((response) => response.arrayBuffer());
+  player.loadArrayBuffer(midi);
+};
+
 const Solved = ({ model }: Props) => {
   const onReset = () => dispatch({ type: "INIT" });
   const { count: moves, nrTiles: tiles } = model.data;
   const append = (nr: number) => (nr > 1 ? "s" : "");
+  const player = new MidiPlayer.Player();
+  const play = async () => {
+    await load(player);
+    player.play();
+  };
+
   return (
     <SolvedDiv>
       <div>Solved!</div>
+      <div>
+        <button onClick={() => load(player)}>Load</button>
+      </div>
+      <div>
+        <button onClick={play}>Play</button>
+      </div>
       <div>{`It took you ${moves} move${append(
         moves
       )} to solve the Problem with ${tiles} tile${append(tiles)}.`}</div>
       <div>
         <button onClick={onReset}>Reset</button>
+      </div>
+      <div>
+        <button>Start</button>
       </div>
     </SolvedDiv>
   );
